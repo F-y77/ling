40864a887f350a2b85aaaa8a2b8b3bcb29184a0f
Va6gn <2810727328@qq.com>
2025-03-27 15:24:36 +0800

    V0.5

--- modinfo.lua
+++ modinfo.lua
@@ -2,7 +2,7 @@
 name = "绫"
 description = "ling "
 author = "Va6gn"
-version = "0.4" -- This is the version of the template. Change it to your own number.
+version = "0.5" -- This is the version of the template. Change it to your own number.
 
 -- This is the URL name of the mod's thread on the forum; the part after the ? and before the first & in the url
 forumthread = "/files/file/950-extended-sample-character/"
